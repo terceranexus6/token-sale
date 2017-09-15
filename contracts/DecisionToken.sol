1fbfaf83cf9946d98b52a2a5d0eb18d8ed5da6b3
nimo <nimo@ipayroll.co.nz>
2017-09-15 16:24:32 +1200

    Prepare for audit, fixed bug with token claculation, cleaned clutter and added destroy method and add-in-bulk

--- contracts/DecisionToken.sol
+++ contracts/DecisionToken.sol
@@ -2,42 +2,45 @@ pragma solidity ^0.4.13;
 
 /**
  * Horizon State Decision Token Contract
- * 
- * Version 0.8
+ *
+ * Version 0.9
  *
  * Author Nimo Naamani
  *
- * This smart contract code is Copyright 2017 HorizonState (https://Horizonstate.com)
+ * This smart contract code is Copyright 2017 Horizon State (https://Horizonstate.com)
  *
  * Licensed under the Apache License, version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  *
  **/
 
 import "./zeppelin-solidity/contracts/token/MintableToken.sol";
+import "./zeppelin-solidity/contracts/math/SafeMath.sol";
 
 /*
 * @title Horizon State Token
-* @dev ERC20 Decision Token (DCT)
+* @dev ERC20 Decision Token (HST)
 * @author Nimo Naamani
 *
-* DCT Tokens have 18 decimal places. The smallest meaningful (and transferable)
-* unit is therefore 0.000000000000000001 DCT. This unit is called a 'danni'.
+* HST tokens have 18 decimal places. The smallest meaningful (and transferable)
+* unit is therefore 0.000000000000000001 HST. This unit is called a 'danni'.
 *
-* 1 DCT = 1 * 10**18 = 1000000000000000000 dannis.
+* 1 HST = 1 * 10**18 = 1000000000000000000 dannis.
 *
-* Maximum total DCT supply is 1 Billion.
+* Maximum total HST supply is 1 Billion.
 * This is equivalent to 1000000000 * 10**18 = 1e27 dannis.
 *
-* DCT are mintable on demand (as they are being purchased), which means that
+* HST are mintable on demand (as they are being purchased), which means that
 * 1 Billion is the maximum.
 */
 contract DecisionToken is MintableToken {
+
+  using SafeMath for uint256;
 
   // Name to appear in ERC20 wallets
   string public constant name = "Decision Token";
 
   // Symbol for the Decision Token to appear in ERC20 wallets
-  string public constant symbol = "DCT";
+  string public constant symbol = "HST";
 
   // Version of the source contract
   string public constant version = "1.0";
@@ -57,73 +60,33 @@ contract DecisionToken is MintableToken {
   /**
    * @dev modifier to allow actions only when the token can be released
    */
-  modifier whenReleased() {
+  modifier onlyWhenReleased() {
+    require(mintingFinished);
     require(now >= releaseTime);
     _;
   }
 
-
   /**
   * @dev Constructor for the DecisionToken.
-  * Assign the tokenReserve to Horizon State.
+  *
+  * The contract shall assign the initial tokenReserve to Horizon State.
   */
   function DecisionToken(uint256 _releaseTime) MintableToken() {
     require(releaseTime > now);
+    releaseTime = _releaseTime;
     owner = msg.sender;
     totalSupply = tokenReserve;
     balances[owner] = totalSupply;
     Mint(owner, totalSupply);
-    releaseTime = _releaseTime;
-  }
-
-  /**
-   * @dev Function to mint tokens
-   * Override StandardToken to return true/false rather than throw and ad
-   * a Transfer event from 0x0 to owner.
-   * @param _to The address that will receive the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool success) {
-    if (totalSupply.add(_amount) > tokenCap) {
-      return false;
-    }
-    totalSupply = totalSupply.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
-    Transfer(0x0, owner, _amount);
-    Transfer(owner, _to, _amount);
-    return true;
-  }
-
-  /**
-   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-   * Override StandardToken to return true/false rather than throw
-   * @param _spender The address which will spend the funds.
-   * @param _value The amount of tokens to be spent.
-   */
-  function approve(address _spender, uint256 _value) returns (bool success) {
-
-    // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender, 0)` if it is not
-    //  already 0 to mitigate the race condition described here:
-    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    if ((_value == 0) || (allowed[msg.sender][_spender] == 0)) {
-      allowed[msg.sender][_spender] = _value;
-      Approval(msg.sender, _spender, _value);
-      return true;
-    } else {
-      return false;
-    }
   }
 
   // @dev override the transfer() function to only work when released
-  function transfer(address _to, uint256 _value) whenReleased returns (bool) {
+  function transfer(address _to, uint256 _value) onlyWhenReleased returns (bool) {
     return super.transfer(_to, _value);
   }
 
   // @dev override the transferFrom() function to only work when released
-  function transferFrom(address _from, address _to, uint256 _value) whenReleased returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) onlyWhenReleased returns (bool) {
     return super.transferFrom(_from, _to, _value);
   }
 }
--- contracts/DecisionTokenSale.sol
+++ contracts/DecisionTokenSale.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.13;
 /**
  * Horizon State Token Sale Contract
  *
- * Version 0.8
+ * Version 0.9
  *
  * Author Nimo Naamani
  *
- * This smart contract code is Copyright 2017 HorizonState (https://Horizonstate.com)
+ * This smart contract code is Copyright 2017 Horizon State (https://Horizonstate.com)
  *
  * Licensed under the Apache License, version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -26,22 +26,23 @@ pragma solidity ^0.4.13;
 contract DecisionTokenSale is Ownable {
   using SafeMath for uint256;
 
-  // Sale starts at 2017-10-02T00:00:00+00:00
-  uint32 public constant saleStart = 1506902400;
+  // Start timestamp where investments are open to the public.
+  // Before this timestamp - only whitelisted addresses allowed to buy.
+  uint256 public startTime;
 
-  // Sale ends 15 days later 2017-10-17T00:00:00+00:00
-  uint32 public constant saleEnd = saleStart + 15 days;
+  // End time. investments can only go up to this timestamp.
+  uint256 public endTime;
 
-  // Early bird buyers receive this many tokens per ETH
-  uint16 public constant earlyBirdTokenRate = 3500;
+  // Whitelisted and 1st day buyers receive this many tokens per ETH
+  uint256 public constant earlyBirdTokenRate = 3500;
 
   // Day 2-8 buyers receive this many tokens per ETH
-  uint16 public constant secondStageTokenRate = 3250;
+  uint256 public constant secondStageTokenRate = 3250;
 
   // Day 9-16 buyers receive this many tokens per ETH
-  uint16 public constant thirdStageTokenRate = 3000;
+  uint256 public constant thirdStageTokenRate = 3000;
 
-  // The DecisionToken that is sold with this token sale
+  // The Decision Token that is sold with this token sale
   MintableToken public token;
 
   // The address where the funds are kept
@@ -61,30 +62,29 @@ contract DecisionTokenSale is Ownable {
    * @param amount    - Number of tokens purchased
    */
   event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount);
+  event LogUserAddedToWhiteList(address indexed user);
+  event LogUserUserRemovedFromWhiteList(address indexed user);
 
   /*
-  * @dev  Constructor
+  * @dev Constructor
   * @param _wallet - The wallet where the token sale proceeds are to be stored
   */
-  function DecisionTokenSale(address _wallet) {
-      require(saleStart >= now);
-      require(saleEnd >= saleStart);
+  function DecisionTokenSale(uint256 _startTime, uint256 _endTime, MintableToken _token, address _wallet) {
+      require(_startTime >= now);
+      require(_endTime >= _startTime);
       require(_wallet != 0x0);
-      token = createTokenContract();
+      startTime = _startTime;
+      endTime = _endTime;
+      token = _token;
       wallet = _wallet;
       weiRaised = 0;
-  }
-
-  // This function just creates the new DecisionToken contract, and
-  // sets its owner to this contract.
-  function createTokenContract() internal returns (MintableToken) {
-    return new DecisionToken(saleEnd + 7 days);
   }
 
   /// This is the function to use for buying tokens.
   function buyTokens() payable {
     require(msg.sender != 0x0);
     require(validPurchase(msg.sender));
+
     uint256 weiAmount = msg.value;
 
     // Calculate token amount to be created
@@ -103,14 +103,17 @@ contract DecisionTokenSale is Ownable {
     wallet.transfer(msg.value);
   }
 
-  // @return true if the transaction can buy tokens
-  function validPurchase(address buyer) internal constant returns (bool) {
-    bool withinPeriod = whiteListedForPresale[buyer] || now >= saleStart;
-    bool nonZeroPurchase = msg.value != 0;
-    return !hasEnded() && withinPeriod && nonZeroPurchase;
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens();
   }
 
-
+  // @return true if the transaction can buy tokens
+  function validPurchase(address buyer) internal constant returns (bool) {
+    bool saleHasOpenedForBuyer = whiteListedForPresale[buyer] || now >= startTime;
+    bool nonZeroPurchase = msg.value != 0;
+    return saleHasOpenedForBuyer && !hasEnded() && nonZeroPurchase;
+  }
 
   // The token sale prefers early birds, as per the token sale whitepaper and
   // Horizon State's Token Sale page.
@@ -118,33 +121,45 @@ contract DecisionTokenSale is Ownable {
   // Days 2-8  : 3250 tokens per Ether
   // Days 9-16 : 3000 tokens per Ether
   function findTokenAmount(uint256 weiAmount) internal constant returns (uint256) {
-    if (now >= saleStart + 8 days) {
+    if (now >= startTime + 8 days) {
       return weiAmount.mul(thirdStageTokenRate);
     }
-    if (now >= saleEnd + 1 days) {
+    if (now >= startTime + 1 days) {
       return weiAmount.mul(secondStageTokenRate);
     }
     return weiAmount.mul(earlyBirdTokenRate);
   }
 
-  /// This is a utility function to help consumers figure out whether the sale
+  /// @dev This is a utility function to help consumers figure out whether the sale
   /// has already ended.
   /// @return true if crowdsale event has ended
   function hasEnded() public constant returns (bool) {
-    return token.mintingFinished() || now > saleEnd;
+    return token.mintingFinished() || now > endTime;
   }
 
-  // @dev Allow the owner of this contract to white list a buyer. White listed
-  // buyers may buy in the presale, i.e before the sale starts.
-  function whiteListAddress(address buyer) onlyOwner returns (bool) {
+  // @dev Allow the owner of this contract to whitelist a buyer.
+  // Whitelisted buyers may buy in the presale, i.e before the sale starts.
+  function whiteListAddress(address buyer) onlyOwner {
     whiteListedForPresale[buyer] = true;
-    return whiteListedForPresale[buyer];
+    LogUserAddedToWhiteList(buyer);
+  }
+
+  // @dev Allow the owner of this contract to whitelist multiple buyers in batch.
+  // Whitelisted buyers may buy in the presale, i.e before the sale starts.
+  function addWhiteListedAddressesInBatch(address[] buyers) onlyOwner {
+    require(buyers.length < 100);
+    for (uint i = 0; i < buyers.length; i++) {
+      whiteListAddress(buyers[i]);
+    }
   }
 
   // @dev Allow the owner of this contract to remove a buyer from the white list.
-  // buyers may buy in the presale, i.e before the sale starts.
-  function removeWhiteListedAddress(address buyer) onlyOwner returns (bool) {
+  function removeWhiteListedAddress(address buyer) onlyOwner {
     whiteListedForPresale[buyer] = false;
-    return whiteListedForPresale[buyer];
+  }
+
+  // @dev Allow the owner of this contract to terminate it
+  function destroy() onlyOwner {
+    selfdestruct(owner);
   }
 }
